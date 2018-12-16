a4ddc08344703fdce44fca3cf1b3435be9cc1b0e
Ramona Mihai <ramona.mihai87@yahoo.com>
2018-12-16 12:17:39 +0200

    Working with git

--- OOP_Homework2/Program.cs
+++ OOP_Homework2/Program.cs
@@ -7,7 +7,7 @@ namespace OOP_Homework2
         static void Main(string[] args)
         {
             DateTime now = DateTime.Now;
-            Rabbit r = new Rabbit("blue", "grey", now.AddYears(-1), true);
+            Rabbit r = new Rabbit("blue", "grey", now.AddYears(-2), true);
             Console.WriteLine("Details about my rabbit: " + r);
             Console.WriteLine("Age of the rabbit: " + r.Age + " days");
         }
